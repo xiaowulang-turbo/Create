fa7b19d5d3e373e4a0d2550a22bd2ae040996f5f
Xiaowu <93776841+xiaowulang-turbo@users.noreply.github.com>
2024-01-27 14:39:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const headerImgs = document.querySelectorAll(".header__img");
+
+let curImg = 0;
+
+const gotoImg = function () {
+  headerImgs.forEach((img, i) => {
+    //console.log(curImg);
+    if (curImg === 4) {
+      curImg = 0;
+    }
+    img.style.transform = `translateX(${100 * (i - curImg)}%`;
+  });
+  curImg++;
+};
+gotoImg();
+
+const refreshImg = function () {
+  setTimeout(function () {
+    gotoImg();
+
+    refreshImg();
+  }, 4000);
+};
+
+refreshImg();
