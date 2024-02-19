c4338b917f9efe4b82d79f7b7e3f156c49d5fa30
Xiaowu <93776841+xiaowulang-turbo@users.noreply.github.com>
2024-02-19 11:53:48 +0800

    Add files via upload

--- script-2.js
+++ script-2.js
@@ -1,10 +1,20 @@
 "use strict";
 const topicChoose1 = document.querySelector("#img__topic--1");
+const topicChoose2 = document.querySelector("#img__topic--2");
+const topicChoose3 = document.querySelector("#img__topic--3");
 const topicChoose4 = document.querySelector("#img__topic--4");
 
 topicChoose1.addEventListener("click", () => {
   window.location.href = "./third.html";
 });
+topicChoose2.addEventListener("click", () => {
+  alert("敬请期待！");
+});
+
+topicChoose3.addEventListener("click", () => {
+  alert("敬请期待！");
+});
+
 topicChoose4.addEventListener("click", () => {
   window.location.href = "./forth.html";
 });
--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 
 const headerImgs = document.querySelectorAll(".header__img");
 // const countryChoose = document.getElementById("img__country--2");
-const countryChoose1 = document.querySelector("#img__country--2");
+const countryChoose1 = document.querySelector("#img__country--1");
+const countryChoose2 = document.querySelector("#img__country--2");
+const countryChoose3 = document.querySelector("#img__country--3");
 
 let curImg = 0;
 
@@ -30,6 +32,14 @@ const refreshImg = function () {
 // console.log(headerImgs);
 
 // console.log(countryChoose1);
+
 countryChoose1.addEventListener("click", () => {
+  alert("敬请期待！");
+});
+countryChoose2.addEventListener("click", () => {
   window.location.href = "./second.html";
+});
+
+countryChoose3.addEventListener("click", () => {
+  alert("敬请期待！");
 });
