942ffcf831ebc3d5de7972f6d6ea340972012e3a
Xiaowu <93776841+xiaowulang-turbo@users.noreply.github.com>
2024-02-25 19:43:34 +0800

    Add files via upload

--- script-map.js
+++ script-map.js
@@ -13,15 +13,22 @@
 //   .openPopup();
 
 var map = new AMap.Map("container", {
-  zoom: 11, //级别
-  center: [114.3005774, 30.5930122], //中心点坐标
+  zoom: 10, //级别
+  // 25.9938561,
+  center: [99.9018944, 25.9938561], //中心点坐标
   viewMode: "3D", //使用3D视图
 });
 
 // 创建一个 Marker 实例：
 var marker = new AMap.Marker({
-  position: new AMap.LngLat(114.3005774, 30.5930122), // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
-  title: "武汉",
+  position: new AMap.LngLat(99.927, 25.99), // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+  title: "凤羽古镇",
+});
+
+marker.setLabel({
+  offset: new AMap.Pixel(5, -15), //设置文本标注偏移量
+  content: "<div class='info'>凤羽古镇所在地</div>", //设置文本标注内容
+  direction: "right", //设置文本标注方位
 });
 
 // 将创建的点标记添加到已有的地图实例：
